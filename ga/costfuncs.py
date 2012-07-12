6a3ab71885076cb9062db0771279d6365c60317f
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-07-12 17:41:10 -0400

    More complex cost function
    weights still need tweaking. Need to tweak on a real example.

--- ga/costfuncs.py
+++ ga/costfuncs.py
@@ -38,23 +38,24 @@ def biomechanical_cost(chromo):
     # factors which inhibit cost
     num_frets_depressed = 0     # number of frets depressed throughout the tab   
     fret_distance_between = 0   # fret distance between consecutive events
-    fret_distance_surround = 0  # fret distance between event and average of 6 surrounding events
+    fret_distance_context = 0   # fret distance between event and average of 6 surrounding events
     fret_distance_within = 0    # fret distance within chords
     fret_distance_largest = 0   # largest fretwise distance between notes of sequential chords
+    intuitive_penalty = 0       # intuitive costs (higher notes played on thinner strings)
+    cognitive_penalty = 0       # cognitive costs (high notes played higher on fretboard)
 
-    # factors which contribute to cost
-    num_open_strings = 0
+    #num_open_strings = 0
 
     # weights
     w_num_frets_depressed = 1.0  
-    w_fret_distance_between = 1.0  
-    w_fret_distance_surround = 1.0
+    w_fret_distance_between = 5.0  
+    w_fret_distance_context = 1.0
     w_fret_distance_within = 1.0
-    w_fret_distance_largest = 1.0
-    w_num_open_strings = -1.0
+    w_fret_distance_largest = 2.5
+    w_cognitive_penalty = 1.0
+    w_intuitive_penalty = 1.5
 
-    prev_gene = None
-    for i in xrange(chromo.genes):
+    for i in xrange(len(chromo.genes)):
         # get six surrounding events (3 before, current, 3 after)
         context = []
         for j in range(-3,4):
@@ -66,25 +67,93 @@ def biomechanical_cost(chromo):
             elif isinstance(event, Strum):
                 context.append(event.plucks)
 
-        # this variable is for code readability
+        # these variables are for code readability
         cur_strum = context[3]
+        prev_strum = context[2]
 
-        frets_depressed = filter(lambda f: f != 0, [p.fret for p in cur_strum])
+        cur_frets_depressed = filter(lambda f: f != 0, [p.fret for p in cur_strum])
 
-        num_open_strings += len(cur_strum) - len(frets_depressed)
-        num_frets_depressed += len(frets_depressed)
+        #num_open_strings += len(cur_strum) - len(cur_frets_depressed)
+        num_frets_depressed += len(cur_frets_depressed)
 
-        if len(frets_depressed) > 0:
-            fret_avg = reduce(lambda f1,f2: f1+f2, frets_depressed)/len(frets_depressed)
+        if len(cur_frets_depressed) > 0:
+            fret_avg = reduce(lambda f1,f2: f1+f2, cur_frets_depressed)/len(cur_frets_depressed)
 
             # calculate how much the chord frets deviate from the average fret number in the chord
-            fret_distance_within += reduce(lambda f1,f2: f1+f2, map(lambda f: abs(f - fret_avg), frets_depressed))
+            fret_distance_within += reduce(lambda f1,f2: f1+f2, map(lambda f: abs(f - fret_avg), cur_frets_depressed))
                     
-        # calculate fretwise distance between sequential chords
-        if surrounding[2] is not None:
-            prev_frets_depressed = filter(lambda f: f != 0, [p.fret for p in surrounding[2]])
+            # calculate average fret-wise distance between chords in context
+            context_frets_depressed = {'sum': 0, 'len': 0} # more efficient than pushing to a list to find avg
+            for strum in context:
+                if strum is cur_strum:
+                    # distance with itself is always zero
+                    continue
 
-    cost = (w_num_frets_depressed*num_frets_depressed + w_num_open_strings*num_open_strings
-            + w_fret_distance_within*fret_distance_within + w_fret_distance_between*fret_distance_between)
+                if strum is not None:
+                    strum_frets_depressed = filter(lambda f: f != 0, [p.fret for p in strum])
+                    context_frets_depressed['sum'] += sum(strum_frets_depressed)
+                    context_frets_depressed['len'] += len(strum_frets_depressed)
+
+                    if strum is prev_strum and len(strum) == 1 and len(cur_strum) == 1:
+                        # penalize an open pluck followed by a fretted pluck
+                        if prev_strum[0].fret == 0 and cur_strum[0].fret != 0:
+                            intuitive_penalty += 10
+
+                        # check if higher note is played on a lower string
+                        # or lower note is played on a higher string
+                        prev_note = chromo.genes[i-1].score_event
+                        cur_note = chromo.genes[i].score_event
+                        if cur_note > prev_note and cur_strum[0].string < prev_strum[0].string:
+                            intuitive_penalty += 10
+                        elif cur_note < prev_note and cur_strum[0].string > prev_strum[0].string:
+                            intuitive_penalty += 10
+
+                    if len(strum_frets_depressed) > 0:
+                        strum_fret_avg = sum(strum_frets_depressed)/len(strum_frets_depressed)
+                        strum_distance = sum(map(lambda f: abs(f - strum_fret_avg), cur_frets_depressed))
+
+                        # slight penalty to notes played high on the fretboard
+                        if strum_fret_avg > 10:
+                            cognitive_penalty += 10
+
+                        if strum is prev_strum:
+                            # add fret-wise distance between previous strum to cost function
+                            fret_distance_between += strum_distance
+
+                            # add penalty for excessively large fret-wise distances
+                            if strum_distance > 5 * len(strum_frets_depressed): 
+                                # since 5 frets is span of hand
+                                w_fret_distance_between *= 2
+
+                            # total largest fretwise distance between any two notes of sequential chords
+                            max_seq_fret_distance = 0
+                            for p_cur in cur_strum:
+                                for p_prev in prev_strum:
+                                    notewise_distance = abs(p_cur.fret - p_prev.fret)
+                                    if notewise_distance > max_seq_fret_distance:
+                                        max_seq_fret_distance = notewise_distance
+
+                            fret_distance_largest += max_seq_fret_distance
+
+            context_fret_avg = context_frets_depressed['sum']/context_frets_depressed['len']
+            context_distance = sum(map(lambda f: abs(f - context_fret_avg), cur_frets_depressed))
+            fret_distance_context += context_distance
+
+            # add penalty for excessively large context fret-wise differences
+            if context_distance > 5 * len(cur_frets_depressed):
+                # since 5 frets is span of hand
+                w_fret_distance_context *= 2
+
+            # add penalty for 
+
+    cost = (w_num_frets_depressed*num_frets_depressed
+            + w_fret_distance_within*fret_distance_within 
+            + w_fret_distance_between*fret_distance_between
+            + w_fret_distance_largest*fret_distance_largest
+            + w_fret_distance_context*fret_distance_context
+            + w_intuitive_penalty*intuitive_penalty
+            + w_cognitive_penalty*cognitive_penalty)
 
     return cost
+
+
